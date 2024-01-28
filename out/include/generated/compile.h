75f0fcbe86e76de49f7837f5acaff3aee1222ab7
Emiferpro <emiferpro@gmail.com>
2024-01-28 03:37:19 -0300

    viva: misc: add build scripts
    Signed-off-by: Emiferpro <emiferpro@gmail.com>

--- out/include/generated/compile.h
+++
@@ -1,7 +0,0 @@
-/* This file is auto generated, version 0 */
-/* SMP PREEMPT */
-#define UTS_MACHINE "aarch64"
-#define UTS_VERSION "#0 SMP PREEMPT Sun Jan 28 03:30:15 -03 2024"
-#define LINUX_COMPILE_BY "emiferpro"
-#define LINUX_COMPILE_HOST "ubuntu"
-#define LINUX_COMPILER "xRageTC clang version 15.0.0 (https://github.com/llvm/llvm-project 4f97aa7e1d70f1b259e5fddd85de05235b01b192), LLD 15.0.0"
--- out/include/generated/utsrelease.h
+++
@@ -1,1 +0,0 @@
-#define UTS_RELEASE "4.14.186-perf-g611ff2d3ea37"
